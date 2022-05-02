48456a704c0a02c3701e66a5b2585ddbfae68611
EdCanCe <edmundocanedoc.ce120@gmail.com>
2022-05-01 21:21:01 -0500

    Examen Diagnóstico

--- ExamenDiagnostico/ParOImpar.cpp
+++ ExamenDiagnostico/ParOImpar.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 int main()
 {
-    int a;
-    cin>>a;
-    if(a%2==0) cout<<"PAR";
-    else cout<<"IMPAR";
-    return 0;
+    int a, b, c;
+    cin>>a>>b>>c;
+    if(a==b&&b==c){
+        cout<<"equilatero";
+    }else if(a==b||b==c||a==c){
+        cout<<"isosceles";
+    }else cout<<"escaleno";
 }
